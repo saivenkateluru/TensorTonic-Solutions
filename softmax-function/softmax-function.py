6defe50792d327c5b255b85a9d0bae65d9df2853
Sai Venkat Eluru <69706801+saivenkateluru@users.noreply.github.com>
2026-08-17 18:39:44 +0530

    Solve softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -10,5 +10,5 @@ def softmax(x):
     x = np.asarray(x,dtype=float)
     m = np.max(x,axis=-1,keepdims=True)
     shifted = x-m
-    softmx = np.exp(shifted) / (np.sum(np.exp(shifted),axis=-1,keepdims=True))
+    softmx = np.exp(shifted - np.log((np.sum(np.exp(shifted),axis=-1,keepdims=True))))
     return softmx
